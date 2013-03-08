406a3c50a4539d2b8eace9ddde47f172bc49afc8
flem789 <flemmingja@hotmail.com>
2013-03-08 17:44:54 +0100

    ny version

--- HelloWorld/src/HellloJava.java
+++ HelloWorld/src/HellloJava.java
@@ -6,7 +6,7 @@ public class HellloJava {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hej world");
+		System.out.println("hej world....");
 
 	}
 
