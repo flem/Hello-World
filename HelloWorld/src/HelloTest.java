877fd85059ecfac61498b05ce2d19b9ec88defbd
flem789 <flemmingja@hotmail.com>
2013-03-08 17:39:45 +0100

    Testigen

---
+++ HelloWorld/src/HelloTest.java
@@ -0,0 +1,13 @@
+
+public class HelloTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hej TEST");
+
+	}
+
+}
